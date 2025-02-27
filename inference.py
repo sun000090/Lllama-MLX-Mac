38f6289f6e7c1fe00ca178f596acac9283e46ee8
Sundeep Samant Singh <162400043+sun000090@users.noreply.github.com>
2025-02-27 17:36:53 +0530

    Add files via upload

--- inference.py
+++ inference.py
@@ -1,17 +1,50 @@
-import glob
-from logger import Logger
-from mlx_lm import load, generate
-from dataset import csv_creator
+from src.agent_executors import AgentExec
+from logs.logger import Logger
+import os
 
-### Initialize model for inference
-model_name1 = 'unsloth/Llama-3.2-3B-Instruct' #Llama
-model_name2 = 'fused_model_4bit' #Fused model
+logss = Logger().get_logger('Logger')
 
-### Load file
-location = input('Enter file location: ')# Location format should be ./location/file.csv
-files = csv_creator.read_table(location)
-prompt = f'Analyze the company from investor prespective. Financial data {files}'
+class InferenceAgent:
+    _instance = None
 
-### Load model and generate response
-model, tokenizer = load(model_name1)
-response = generate(model, tokenizer, prompt= prompt, max_tokens=1024, temp=0.5, verbose=True)
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(InferenceAgent, cls).__new__(cls)
+        return cls._instance
+
+    @staticmethod
+    def get_instance():
+        if InferenceAgent._instance is None:
+            InferenceAgent()
+        return InferenceAgent._instance
+
+    def __init__(self):
+        if not hasattr(self, 'initialized'):
+            self.agent_executors = self.AgentExecutor()  # Initialize once
+            self.initialized = True
+
+    def AgentExecutor(self):
+        agent_executors = AgentExec(loc_name=os.getenv('CONF_DIR'))
+        agent_executors_ = agent_executors.agent_executor()
+        logss.info("Agent executor loaded")
+        return agent_executors_
+
+    def inferences(self, input_prompt):
+        try:
+            agents = self.agent_executors  # Use the initialized instance
+            events = agents.stream(
+                {"messages": [("user", input_prompt)]},
+                stream_mode="values"
+            )
+            
+            messages = []
+            for event in events:
+                messages.append(event["messages"][-1])
+                logss.info(event["messages"])
+            final_messages = messages[-1].content
+            final_table = messages[-2].content
+            final_query = messages[-3].additional_kwargs['tool_calls'][0]['function']['arguments']
+            return final_messages, final_table, final_query
+        except Exception as e:
+            logss.error(f"Error in dynamic execution: {e}")
+            return None, None, None
